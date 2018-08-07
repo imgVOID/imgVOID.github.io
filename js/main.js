766f31de962db2e35b143b760559d59c3900a264
Maria Gl <imgvoid@i.ua>
2018-08-07 20:10:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
+// Canvas settings for first tab
 var id;
 
 function resizeCanvas(canvas) {
     canvas.height = window.innerHeight;
-    canvas.width  = window.innerWidth;
+    canvas.width = window.innerWidth;
 
-    var ctx = canvas.getContext("2d");
+    var ctx = canvas.getContext('2d');
 
     function stopLines() {
-        if (count>=60){
+        if (count >= 95) {
             clearInterval(id);
         } else {
             setTimeout(stopLines, 100);
         }
 
     }
-    
-    id = setInterval(function(){
+
+    id = setInterval(function() {
         drawRandomLine(ctx);
         count++;
     }, 1);
-    
+
     var count = 0;
-    //setTimeout(id, 1000);
-    //setTimeout(stopLines, 1100);
     setTimeout(id, 300);
     setTimeout(stopLines, 400);
 }
 
 function drawRandomLine(canvas) {
 
-    var a = rangedRandom(1, window.innerWidth+500);
+    var a = rangedRandom(1, window.innerWidth + 500);
     var b = rangedRandom(1, window.innerHeight);
     var c = rangedRandom(1, window.innerWidth);
     var d = rangedRandom(1, window.innerHeight);
 
-    var colors = ["black","dimgray","slategray","darkgray","darkred"];
-    var random = Math.floor(Math.random()*5);
+    var colors = ['black', 'dimgray', 'slategray', 'darkgray', 'darkred'];
+    var random = Math.floor(Math.random() * 5);
 
     canvas.beginPath();
     canvas.lineWidth = rangedRandom(1, 20);
@@ -46,103 +45,139 @@ function drawRandomLine(canvas) {
 }
 
 function rangedRandom(min, max) {
-    return Math.floor(Math.random() * (max - min)+min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-$(document).ready(function(){
-    
-    var c = $("#background")[0];
-    resizeCanvas(c);
-    
-    
-    var classesNav = {
-    edcA : "#navEducation a",
-    portA : "#navPortfolio a",
-    welA : '#navWelcome a',
-    edc : "#education",
-    portf : "#portfolio",
-    wel : "#welcome"
-};
+// function for portfolio tab menu
 
-function clearClasses(){
-    for(var key in classesNav){
-        $(classesNav[key]).removeClass('active black');
+function clear() {
+    $('#clear').addClass('white');
+    workslist = document.getElementById('pis').children;
+    for (i = 1; i <= workslist.length; i++) {
+        $(workslist[i]).removeClass('hidden');
     }
 }
 
-$(classesNav.edcA).click(function () {
-    clearClasses();
-    $(classesNav.edc).addClass('active');
-    $(this).addClass('black')
+// functions for already loaded page
+
+$(document).ready(function() {
+// canvas start!
+    var c = $('#background')[0];
+    resizeCanvas(c);
+//main navigation colors switching
+    var classesNav = {
+        edcA: '.navEducation a',
+        portA: '.navPortfolio a',
+        welA: '.navWelcome a',
+        edc: '#education',
+        portf: '#portfolio',
+        wel: '#welcome'
+    };
+
+    function clearClasses() {
+        for (var key in classesNav) {
+            $(classesNav[key]).removeClass('active black');
+        }
+    }
+
+    $(classesNav.edcA).click(function() {
+        clearClasses();
+        $(classesNav.edc).addClass('active');
+        $(this).addClass('black');
+    });
+
+    $(classesNav.portA).click(function() {
+        clearClasses();
+        $(classesNav.portf).addClass('active');
+        $(this).addClass('black');
+        clear();
+    });
+
+    $(classesNav.welA).click(function() {
+        clearClasses();
+        $(classesNav.wel).addClass('active');
+        $(this).addClass('black');
+    });
+//borders coloring
+    var itemBorder = ['borderDarkgray', 'borderDarkgray', 'borderGray', 'borderDimgray', 'borderGray', 'borderWhitesmoke'];
+
+    function borders(type, start){
+        for (i = start; i <= type.length; i++) {
+            var random = Math.floor(Math.random() * (itemBorder.length));
+            $(type[i]).addClass(itemBorder[random]);
+    }
+    }
+
+    borders(document.getElementById('pis').children, 1);
+    borders(document.getElementById('edis').children, 0);
 });
-
-$(classesNav.portA).click(function () {
-    clearClasses();
-    $(classesNav.portf).addClass('active');
-    $(this).addClass('black')
-});
-
-$(classesNav.welA).click(function () {
-    clearClasses();
-    $(classesNav.wel).addClass('active');
-    $(this).addClass('black')
-});
-
-var itemBorder = ["borderDarkgray","borderDarkgray","borderGray","borderDimgray","borderGray","borderWhitesmoke"];
-
-for (i = 0; i <=itemBorder.length; i++){
-    var random = Math.floor(Math.random()*(itemBorder.length));
-    $('.pi'+(i+1)).addClass(itemBorder[random]);
-}
-
-    document.getElementById('img_block').appendChild(document.getElementById('img_add'));
-    
-});
-
-$(window).resize(function(){
+//canvas resizing
+$(window).resize(function() {
     resizeCanvas(c);
 });
-
-function PopUpShow1(){
-  $("#gmail_me").show();
-}
-function PopUpHide1(){
-  $("#gmail_me").hide();
-}
-
-function PopUpShowAnime(){
-  $("#popup1").show();
-}
-
-function PopUpHide(){
-  for (i=1; i<=8; i++){
-    $("#popup"+i).hide();
-  }
-}
-
-function PopUpShowDjangoBlog(){
-  $("#popup2").show();
+//portfolio tab sorting by keywords
+function filtering(type) {
+    $('#backend').removeClass('black');
+    $('#frontend').removeClass('black');
+    $('#fullstack').removeClass('black');
+    $('#' + type).addClass('black');
+    $('#clear').removeClass('white');
+    var workslist = document.getElementById('pis').children;
+    for (i = 1; i <= workslist.length; i++) {
+        if (workslist[i].classList.contains(type)) {
+            $(workslist[i]).removeClass('hidden');
+        } else {
+            $(workslist[i]).addClass('hidden');
+        }
+    }
 }
 
-function PopUpShowWiki(){
-  $("#popup3").show();
+function frontend() {
+    filtering('frontend');
 }
 
-function PopUpShowCalculator(){
-  $("#popup4").show();
+function fullstack() {
+    filtering('fullstack');
 }
 
-function PopUpShowIphone(){
-  $("#popup5").show();
+function backend() {
+    filtering('backend');
+}
+//email me popup
+function PopUpShow1() {
+    $('#gmail_me').show();
 }
 
-function PopUpShowCodecombat(){
-  $("#popup6").show();
+function PopUpHide1() {
+    $('#gmail_me').hide();
+}
+//portfolio tab popups
+function PopUpShowAnime() {
+    $('#popup1').show();
 }
 
-//[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
-  //img.setAttribute('src', img.getAttribute('data-src'));
-  //img.onload = function() {
-    //img.removeAttribute('data-src');
-  //};
-//});
+function PopUpHide() {
+    for (i = 1; i <= 8; i++) {
+        $('#popup' + i).hide();
+    }
+}
+
+function PopUpShowDjangoBlog() {
+    $('#popup2').show();
+}
+
+function PopUpShowWiki() {
+    $('#popup3').show();
+}
+
+function PopUpShowCalculator() {
+    $('#popup4').show();
+}
+
+function PopUpShowIphone() {
+    $('#popup5').show();
+}
+
+function PopUpShowCodecombat() {
+    $('#popup6').show();
+}

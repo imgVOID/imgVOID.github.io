2356589396c8d8b57c6159182639585e35281f90
Maria Hl <nachtmarie.photo@gmail.com>
2019-01-18 20:22:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Copy email to clipboard
+function copyToClipboard(elementId) {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+}
+
 // Canvas settings for first tab
 var id;
 
